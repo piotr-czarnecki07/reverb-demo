8e44ed5298539f6de075bd848e01c69c8d55553c
Piotr Czarnecki <czarnecki.devv@gmail.com>
2026-08-11 13:27:34 +0200

    feat/create basic gui layout for sliders

--- Demo Reverb/Source/PluginEditor.cpp
+++ Demo Reverb/Source/PluginEditor.cpp
@@ -1,10 +1,48 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-DemoReverbAudioProcessorEditor::DemoReverbAudioProcessorEditor (DemoReverbAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p)
+std::vector<juce::Component*> DemoReverbAudioProcessorEditor::getReverbComponents()
 {
-    setSize (400, 300);
+    return {
+        &roomSizeSlider,
+        &dampingSlider,
+        &dryLevelSlider,
+        &wetLevelSlider,
+        &widthSlider
+    };
+}
+
+std::vector<juce::Component*> DemoReverbAudioProcessorEditor::getEqComponents()
+{
+    return {
+        &lowCutSlider,
+        &highCutSlider
+    };
+}
+
+DemoReverbAudioProcessorEditor::DemoReverbAudioProcessorEditor (DemoReverbAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p),
+    lowCutSliderAttachment(audioProcessor.apvts, "LowCut", lowCutSlider),
+    highCutSliderAttachment(audioProcessor.apvts, "HighCut", highCutSlider),
+    roomSizeSliderAttachment(audioProcessor.apvts, "RoomSize", roomSizeSlider),
+    dampingSliderAttachment(audioProcessor.apvts, "Damping", dampingSlider),
+    dryLevelSliderAttachement(audioProcessor.apvts, "DryLevel", dryLevelSlider),
+    wetLevelSliderAttachment(audioProcessor.apvts, "WetLevel", wetLevelSlider),
+    widthSliderAttachment(audioProcessor.apvts, "Width", widthSlider)
+{
+    setSize (500, 300);
+
+    addAndMakeVisible(reverbSliderGroup);
+    addAndMakeVisible(eqSliderGroup);
+
+    for (auto* comp : getReverbComponents())
+        reverbSliderGroup.addAndMakeVisible(comp);
+
+    for (auto* comp : getEqComponents())
+        eqSliderGroup.addAndMakeVisible(comp);
+
+    reverbSliderGroup.setColour(juce::GroupComponent::outlineColourId, juce::Colours::transparentBlack);
+    eqSliderGroup.setColour(juce::GroupComponent::outlineColourId, juce::Colours::transparentBlack);
 }
 
 DemoReverbAudioProcessorEditor::~DemoReverbAudioProcessorEditor()
@@ -13,14 +51,27 @@ DemoReverbAudioProcessorEditor::~DemoReverbAudioProcessorEditor()
 
 void DemoReverbAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    
 }
 
 void DemoReverbAudioProcessorEditor::resized()
 {
+    reverbSliderGroup.setBounds(20, 110, 460, 90);
 
+    auto reverbArea = reverbSliderGroup.getLocalBounds();
+
+    int w = reverbArea.getWidth() / 5;
+    roomSizeSlider.setBounds(reverbArea.removeFromLeft(w).reduced(5));
+    dampingSlider.setBounds(reverbArea.removeFromLeft(w).reduced(5));
+    dryLevelSlider.setBounds(reverbArea.removeFromLeft(w).reduced(5));
+    wetLevelSlider.setBounds(reverbArea.removeFromLeft(w).reduced(5));
+    widthSlider.setBounds(reverbArea.reduced(5));
+
+    eqSliderGroup.setBounds(20, 220, 460, 70);
+
+    auto eqArea = eqSliderGroup.getLocalBounds();
+
+    w = eqArea.getWidth() / 2;
+    lowCutSlider.setBounds(eqArea.removeFromLeft(w).reduced(5));
+    highCutSlider.setBounds(eqArea.reduced(5));
 }
--- Demo Reverb/Source/PluginEditor.h
+++ Demo Reverb/Source/PluginEditor.h
@@ -3,6 +3,16 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct ReverbSlider : juce::Slider // vertical bar slider, typical for classic reverb plugin style
+{
+    ReverbSlider() : juce::Slider(juce::Slider::SliderStyle::LinearBarVertical, juce::Slider::TextEntryBoxPosition::NoTextBox) {}
+};
+
+struct EqSlider : juce::Slider
+{
+    EqSlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox) {}
+};
+
 class DemoReverbAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
@@ -14,6 +24,32 @@ public:
 
 private:
     DemoReverbAudioProcessor& audioProcessor;
+
+    juce::GroupComponent reverbSliderGroup,
+        eqSliderGroup;
+
+    EqSlider lowCutSlider,
+        highCutSlider;
+    
+    ReverbSlider roomSizeSlider,
+        dampingSlider,
+        dryLevelSlider,
+        wetLevelSlider,
+        widthSlider;
+
+    std::vector<juce::Component*> getReverbComponents();
+    std::vector<juce::Component*> getEqComponents();
+
+    using apvts = juce::AudioProcessorValueTreeState;
+    using Attachment = apvts::SliderAttachment;
+
+    Attachment lowCutSliderAttachment,
+        highCutSliderAttachment,
+        roomSizeSliderAttachment,
+        dampingSliderAttachment,
+        dryLevelSliderAttachement,
+        wetLevelSliderAttachment,
+        widthSliderAttachment;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DemoReverbAudioProcessorEditor)
 };
